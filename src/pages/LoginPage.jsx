c3f682fdd963e6574c1dca45ecc7f65916a0369f
Dominik Sasko <saskodominik@gmail.com>
2022-06-03 14:03:21 +0900

    feat: add LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Page,
   Navbar,
@@ -10,7 +10,20 @@ import {
 } from "framework7-react";
 
 const LoginPage = () => {
-  const [inputs, setInputs] = useState();
+  const [userName, setUserName] = useState({});
+  const [userGender, setUserGender] = useState();
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setUserName((values) => ({ ...values, [name]: value }));
+    console.log(userName);
+  };
+
+  const toggleGender = (gender) => {
+    setUserGender(gender);
+    console.log(userGender);
+  };
 
   return (
     <Page name='login' className='login-page'>
@@ -23,6 +36,53 @@ const LoginPage = () => {
         <p className='login-page__introduction'>
           <b>Let&apos;s Write</b> helps you connect <br /> with likeminded peers
           around the globe!
+        </p>
+      </div>
+
+      <div className='login-page__form-container'>
+        <input
+          className='login-page__username-input'
+          type='text'
+          name='username'
+          placeholder='Username'
+          value={userName.username || ""}
+          onChange={handleChange}
+        />
+
+        <div className='login-page__gender-container'>
+          <div
+            className={
+              "login-page__male-button " +
+              (userGender === "male" ? "is-active" : "")
+            }
+            onClick={() => toggleGender("male")}
+          >
+            <img
+              className='login-page__male-icon'
+              src='../static/male_icon.png'
+              alt='male_icon'
+            />
+          </div>
+          <div
+            className={
+              "login-page__female-button " +
+              (userGender === "female" ? "is-active" : "")
+            }
+            onClick={() => toggleGender("female")}
+          >
+            <img
+              className='login-page__female-icon'
+              src='../static/female_icon.png'
+              alt='female_icon'
+            />
+          </div>
+        </div>
+
+        <button className='login-page__button'>Login</button>
+
+        <p className='login-page__app-info'>
+          Created by Asial Corporation <br />
+          App Version: 1.0.0
         </p>
       </div>
     </Page>
